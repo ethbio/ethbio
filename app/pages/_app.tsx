750a02925800c1da0a62102b4f872be9fdc4fdb6
Shreyas <93078655+sh3y@users.noreply.github.com>
2022-02-15 08:55:45 -0600

    feat: meta tags (open graph)

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -5,7 +5,7 @@ import {
   ErrorFallbackProps,
   useQueryErrorResetBoundary,
 } from "blitz";
-
+import Head from "next/head"
 import { ChakraProvider } from '@chakra-ui/react';
 
 import "app/core/styles/index.css";
@@ -14,6 +14,13 @@ export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
+    <Head>
+      <meta name="og:description" content="A one-page personal bio, powered by web 3.0 technology." />
+      <meta name="og:url" content="https://ethbio.xyz" />
+      <meta name="og:image" content="https://media.discordapp.net/attachments/930160554243395604/943157909548834826/Untitled_design.png?width=477&height=477"/>
+      <meta name="og:site_name" content="Ethbio"/>
+      <meta name="og:title" content="Coming Soon"/>     
+   </Head>
     <ErrorBoundary
       FallbackComponent={RootErrorFallback}
       onReset={useQueryErrorResetBoundary().reset}
